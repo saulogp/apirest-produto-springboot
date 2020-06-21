57b54de5e2f388c539520b2a582ca21e6e8b7998
saulogp <sauloproetti@gmail.com>
2020-06-21 00:28:12 -0300

    metodos get e post mas o post

--- src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
+++ src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
@@ -4,5 +4,5 @@ import com.produtos.apirest.models.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-
+    Produto findById(long id);
 }
---
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -0,0 +1,30 @@
+package com.produtos.apirest.resources;
+import com.produtos.apirest.models.Produto;
+import com.produtos.apirest.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+public class ProdutoResource {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    @GetMapping("/produtos")
+    public List<Produto> listaProdutos(){
+        return produtoRepository.findAll();
+    }
+    @GetMapping("/produto/{id}")
+    public Produto listaProdutoById(@PathVariable(value = "id")long id){
+        return  produtoRepository.findById(id);
+    }
+
+    @PostMapping("/produto")
+    public Produto salvaProduto(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+}
